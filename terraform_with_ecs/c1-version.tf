8ae49d7b0995b9b85560d6f245c15d4a41e145ad
Vamshi_Kesari <118153587+kesarivamshi@users.noreply.github.com>
2022-12-06 06:12:00 +0530

    Update c1-version.tf

--- terraform_with_ecs/c1-version.tf
+++ terraform_with_ecs/c1-version.tf
@@ -6,8 +6,22 @@ provider "aws" {
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "4.33.0"
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 1.1.0"
+
+  cloud {
+    organization = "Kesarivamshi"
+
+    workspaces {
+      name = "terrafrom"
     }
   }
 }
+
